abfb35ecb26f9e991f93211ef546cbfcb43ffbff
Injustive <39840383+Injustive@users.noreply.github.com>
2021-12-05 04:00:09 +0200

    Update screen.py

--- utils/screen.py
+++ utils/screen.py
@@ -8,13 +8,30 @@ from selenium.common.exceptions import TimeoutException
 from .queries_to_server import get_valid_access
 from errors import BadStatusError
 import asyncio
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 SITE_URL = 'https://guessit-space.herokuapp.com/'
 
 
 async def make_screen(user_id, url, area, is_general_stat):
     token = await get_valid_access(user_id)
-    driver = webdriver.Firefox()
+    
+    options = webdriver.FirefoxOptions()
+	options.log.level = "trace"
+
+	options.add_argument("-remote-debugging-port=9224")
+	options.add_argument("-headless")
+	options.add_argument("-disable-gpu")
+	options.add_argument("-no-sandbox")
+
+	binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
+
+	driver = webdriver.Firefox(
+		firefox_binary=binary,
+		executable_path=os.environ.get('GECKODRIVER_PATH'),
+		options=options)
+
 
     def interceptor(request):
         request.headers['Authorization'] = f'Bearer {token}'
